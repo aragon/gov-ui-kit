502e76ab37d17aa24fc8fc70fe2e93b504dbc497
Kevin Davis <whomst@kevind.xyz>
2024-10-31 16:33:50 +0100

    chore: ts, lint, prettify

--- src/modules/components/proposal/proposalVoting/proposalVotingContainer/proposalVotingContainer.test.tsx
+++ src/modules/components/proposal/proposalVoting/proposalVotingContainer/proposalVotingContainer.test.tsx
@@ -6,8 +6,6 @@ import { type IProposalVotingContainerProps, ProposalVotingContainer } from './p
 describe('<ProposalVotingContainer /> component', () => {
     const createTestComponent = (props?: Partial<IProposalVotingContainerProps>) => {
         const completeProps: IProposalVotingContainerProps = {
-            title: '',
-            description: '',
             ...props,
         };
 
