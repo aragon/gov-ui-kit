e972cb6c88cad2667b47b1c77fb522ca5100eb60
Kevin Davis <whomst@kevind.xyz>
2024-11-12 13:49:03 +0700

    chore: update term text styles

--- src/core/components/definitionList/definitionListItem/definitionListItem.tsx
+++ src/core/components/definitionList/definitionListItem/definitionListItem.tsx
@@ -19,7 +19,7 @@ export const DefinitionListItem: React.FC<IDefinitionListItemProps> = (props) =>
             )}
             {...otherProps}
         >
-            <dt className="line-clamp-1 block overflow-hidden text-base font-semibold text-neutral-800 md:line-clamp-none">
+            <dt className="line-clamp-1 text-base font-normal leading-relaxed text-neutral-800 md:line-clamp-none">
                 {term}
             </dt>
             <dd className="text-base font-normal text-neutral-700 md:pl-4">{children}</dd>
