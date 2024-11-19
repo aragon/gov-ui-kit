df566d339192d1e846a159e1f4b80b6056fb8ae7
Ruggero Cino <ruggero.cino@gmail.com>
2024-11-19 18:04:46 +0100

    Rename proposal-actions-utils file

---
+++ src/modules/components/proposal/proposalActions/proposalActionsItem/proposalActionsItemUtils.ts
@@ -0,0 +1,35 @@
+import { type IProposalAction, ProposalActionType } from '../proposalActionsDefinitions';
+import type {
+    IProposalActionChangeMembers,
+    IProposalActionChangeSettings,
+    IProposalActionTokenMint,
+    IProposalActionUpdateMetadata,
+    IProposalActionWithdrawToken,
+} from '../proposalActionsList';
+
+class ProposalActionsItemUtils {
+    isWithdrawTokenAction = (action: Partial<IProposalAction>): action is IProposalActionWithdrawToken => {
+        return action.type === ProposalActionType.WITHDRAW_TOKEN;
+    };
+
+    isChangeMembersAction = (action: Partial<IProposalAction>): action is IProposalActionChangeMembers => {
+        return action.type === ProposalActionType.ADD_MEMBERS || action.type === ProposalActionType.REMOVE_MEMBERS;
+    };
+
+    isUpdateMetadataAction = (action: Partial<IProposalAction>): action is IProposalActionUpdateMetadata => {
+        return action.type === ProposalActionType.UPDATE_METADATA;
+    };
+
+    isTokenMintAction = (action: Partial<IProposalAction>): action is IProposalActionTokenMint => {
+        return action.type === ProposalActionType.TOKEN_MINT;
+    };
+
+    isChangeSettingsAction = (action: Partial<IProposalAction>): action is IProposalActionChangeSettings => {
+        return (
+            action.type === ProposalActionType.CHANGE_SETTINGS_MULTISIG ||
+            action.type === ProposalActionType.CHANGE_SETTINGS_TOKENVOTE
+        );
+    };
+}
+
+export const proposalActionsItemUtils = new ProposalActionsItemUtils();
