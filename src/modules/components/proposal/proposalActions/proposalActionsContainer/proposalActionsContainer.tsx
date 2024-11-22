a52ee1ae1f02b726efc7d7282946430cda0720d6
Ruggero Cino <ruggero.cino@gmail.com>
2024-11-22 16:43:56 +0100

    Apply changes on accordion component

--- src/modules/components/proposal/proposalActions/proposalActionsContainer/proposalActionsContainer.tsx
+++ src/modules/components/proposal/proposalActions/proposalActionsContainer/proposalActionsContainer.tsx
@@ -1,11 +1,10 @@
-import classNames from 'classnames';
 import React, { Children, useEffect, type ComponentProps } from 'react';
-import { Accordion, Card, EmptyState } from '../../../../../core';
+import { Accordion, EmptyState } from '../../../../../core';
 import { useGukModulesContext } from '../../../gukModulesProvider';
 import { useProposalActionsContext } from '../proposalActionsContext';
 import type { IProposalActionsItemProps } from '../proposalActionsItem';
 
-export interface IProposalActionsContainerProps extends ComponentProps<'div'> {
+export interface IProposalActionsContainerProps extends Omit<ComponentProps<'div'>, 'defaultValue'> {
     /**
      * Custom description for the empty state.
      */
@@ -27,22 +26,25 @@ export const ProposalActionsContainer: React.FC<IProposalActionsContainerProps> 
     const handleAccordionValueChange = (value: string[] = []) => setExpandedActions(value);
 
     return (
-        <Card className={classNames('w-full overflow-hidden', className)} {...otherProps}>
-            <Accordion.Container isMulti={true} value={expandedActions} onValueChange={handleAccordionValueChange}>
-                {actionsCount === 0 && (
-                    <EmptyState
-                        heading={copy.proposalActionsContainer.emptyHeader}
-                        description={emptyStateDescription}
-                        isStacked={false}
-                        objectIllustration={{ object: 'SMART_CONTRACT' }}
-                    />
-                )}
-                {processedChildren.map((child, index) =>
-                    React.isValidElement<IProposalActionsItemProps>(child)
-                        ? React.cloneElement(child, { ...child.props, index })
-                        : child,
-                )}
-            </Accordion.Container>
-        </Card>
+        <Accordion.Container
+            isMulti={true}
+            value={expandedActions}
+            onValueChange={handleAccordionValueChange}
+            {...otherProps}
+        >
+            {actionsCount === 0 && (
+                <EmptyState
+                    heading={copy.proposalActionsContainer.emptyHeader}
+                    description={emptyStateDescription}
+                    isStacked={false}
+                    objectIllustration={{ object: 'SMART_CONTRACT' }}
+                />
+            )}
+            {processedChildren.map((child, index) =>
+                React.isValidElement<IProposalActionsItemProps>(child)
+                    ? React.cloneElement(child, { ...child.props, index })
+                    : child,
+            )}
+        </Accordion.Container>
     );
 };
