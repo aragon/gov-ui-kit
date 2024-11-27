c8fbf5aa2b10b7c038c0e449150423e6dd4837e9
shan8851 <asamshan456@gmail.com>
2024-11-27 21:55:28 +0000

    chore: Add stories for proposal voting progress

---
+++ src/modules/components/proposal/proposalVoting/proposalVotingProgress/proposalVotingProgress.stories.tsx
@@ -0,0 +1,52 @@
+// ProposalVotingProgressItem.stories.tsx
+import type { Meta, StoryObj } from '@storybook/react';
+import { ProposalVotingProgress } from '../index';
+
+const meta: Meta<typeof ProposalVotingProgress.Item> = {
+    title: 'Modules/Components/Proposal/ProposalVoting/ProposalVotingProgress.Item',
+    component: ProposalVotingProgress.Item,
+    parameters: {
+        design: {
+            type: 'figma',
+            url: 'ance-UI-Kit?node-id=16738-17734&m=dev',
+        },
+    },
+};
+
+type Story = StoryObj<typeof ProposalVotingProgress.Item>;
+
+/**
+ * Default usage example of the ProposalVotingProgressItem component.
+ */
+export const TokenVotingSupport: Story = {
+    args: {
+        name: 'Support',
+        description: {
+            value: '75',
+            text: 'of 100 ETH',
+        },
+        value: 75,
+        thresholdIndicator: 50,
+        variant: 'primary',
+        showPercentage: true,
+        showStatusIcon: true,
+    },
+};
+
+/**
+ * Default usage example of the ProposalVotingProgressItem component.
+ */
+export const MultisigApproval: Story = {
+    args: {
+        name: 'Minimum Approval',
+        description: {
+            value: '2',
+            text: 'of 5 members',
+        },
+        value: 40,
+        variant: 'neutral',
+        showStatusIcon: true,
+    },
+};
+
+export default meta;
