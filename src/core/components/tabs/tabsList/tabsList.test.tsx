0c751d1ddbf4f8665b64b94a9632e74b0107780c
shan8851 <asamshan456@gmail.com>
2025-02-17 11:53:23 +0000

    chore: update tests

--- src/core/components/tabs/tabsList/tabsList.test.tsx
+++ src/core/components/tabs/tabsList/tabsList.test.tsx
@@ -1,39 +1,29 @@
 import { render, screen } from '@testing-library/react';
-import { Tabs, type ITabsListProps } from '../../tabs';
+import { Tabs } from '../../tabs';
+import type { ReactNode } from 'react';
 
 describe('<Tabs.List /> component', () => {
-    const createSingleTabTestComponent = (props?: Partial<ITabsListProps>) => {
-        const completeProps: ITabsListProps = { ...props };
-        return (
-            <Tabs.Root>
-                <Tabs.List {...completeProps}>
-                    <Tabs.Trigger label="Tab 1" value="1" />
-                </Tabs.List>
-            </Tabs.Root>
-        );
-    };
-    const createMultiTabsTestComponent = (props?: Partial<ITabsListProps>) => {
-        const completeProps: ITabsListProps = { ...props };
-        return (
-            <Tabs.Root>
-                <Tabs.List {...completeProps}>
-                    <Tabs.Trigger label="Tab 1" value="1" />
-                    <Tabs.Trigger label="Tab 2" value="2" />
-                </Tabs.List>
-            </Tabs.Root>
-        );
-    };
+    const createTestComponent = (children: ReactNode) => (
+        <Tabs.Root>
+            <Tabs.List>{children}</Tabs.List>
+        </Tabs.Root>
+    );
 
     it('should render multiple tab triggers without crashing', () => {
-        render(createMultiTabsTestComponent());
-
+        render(
+            createTestComponent(
+                <>
+                    <Tabs.Trigger label="Tab 1" value="1" />
+                    <Tabs.Trigger label="Tab 2" value="2" />
+                </>,
+            ),
+        );
         expect(screen.getByText('Tab 1')).toBeInTheDocument();
         expect(screen.getByText('Tab 2')).toBeInTheDocument();
     });
 
     it('should render null when only a single tab trigger is present', () => {
-        render(createSingleTabTestComponent());
-
+        render(createTestComponent(<Tabs.Trigger label="Tab 1" value="1" />));
         expect(screen.queryByText('Tab 1')).not.toBeInTheDocument();
     });
 });
