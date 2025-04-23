0e8e099cdc074ba228208634aa60b8e24abaac05
Ruggero Cino <ruggero.cino@gmail.com>
2025-04-23 17:30:33 +0200

    Migrate shake animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,25 +3,6 @@ module.exports = {
     content: ['./src/**/*.{jsx,ts,tsx,mdx}', './docs/**/*.{jsx,tsx,mdx}', '.storybook/*.{jsx,tsx}'],
     theme: {
         extend: {
-            animation: {
-                shake: 'shake 0.82s cubic-bezier(0.36,0.07,0.19,0.97) both',
-            },
-            keyframes: {
-                shake: {
-                    '10%, 90%': {
-                        transform: 'translate3d(-1px, 0, 0)',
-                    },
-                    '20%, 80%': {
-                        transform: 'translate3d(2px, 0, 0)',
-                    },
-                    '30%, 50%, 70%': {
-                        transform: 'translate3d(-4px, 0, 0)',
-                    },
-                    '40%, 60%': {
-                        transform: 'translate3d(4px, 0, 0)',
-                    },
-                },
-            },
             backgroundImage: {
                 'modal-overlay': 'linear-gradient(180deg, rgba(245, 247, 250, 0) 0%, #F5F7FA 100%)',
                 'modal-header': 'linear-gradient(180deg, #F5F7FA 0%, rgba(245, 247, 250, 0) 100%)',
