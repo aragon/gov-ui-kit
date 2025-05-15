e9ec20320c5afd427a0a5dce20b30667ad2177c2
Milos Dzepina <milos@aragon.org>
2025-05-15 17:55:10 +0200

    Update link to add isExternal and showUrl flags
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/core/components/link/link/link.tsx
+++ src/core/components/link/link/link.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import React from 'react';
-import { Icon } from '../../icon';
+import { Icon, IconType } from '../../icon';
 import { LinkBase } from '../linkBase';
 import type { ILinkProps, LinkVariant } from './link.api';
 
@@ -23,14 +23,15 @@ export const Link = React.forwardRef<HTMLAnchorElement, ILinkProps>((props, ref)
         children,
         disabled = false,
         variant = 'primary',
-        description,
         href,
-        iconRight,
+        isExternal = false,
+        iconRight = isExternal ? IconType.LINK_EXTERNAL : undefined,
+        target = isExternal ? '_blank' : undefined,
         onClick,
         className,
         textClassName,
-        target,
         rel,
+        showUrl,
         ...otherProps
     } = props;
 
@@ -59,13 +60,19 @@ export const Link = React.forwardRef<HTMLAnchorElement, ILinkProps>((props, ref)
             aria-disabled={disabled}
             {...otherProps}
         >
-            <div className="flex items-center gap-x-2">
+            <div className="flex items-center gap-x-1.5">
                 <span className={innerTextClassName}>{children}</span>
                 {iconRight && <Icon icon={iconRight} size="sm" />}
             </div>
-            {description && (
-                <p className={classNames('truncate', disabled ? 'text-neutral-300' : 'text-neutral-500')}>
-                    {description}
+            {showUrl && href && (
+                <p
+                    className={classNames(
+                        'truncate text-xs',
+                        'md:text-sm',
+                        disabled ? 'text-neutral-300' : 'text-neutral-500',
+                    )}
+                >
+                    {href}
                 </p>
             )}
         </LinkBase>
