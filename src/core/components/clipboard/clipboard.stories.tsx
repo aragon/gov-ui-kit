7bc9f93b30a37360afa82a3d1abf3936289a1d5e
Milos Dzepina <milos@aragon.org>
2025-05-16 11:08:52 +0200

    Add clipboard stories with link and text
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/core/components/clipboard/clipboard.stories.tsx
+++ src/core/components/clipboard/clipboard.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { Link } from '../link';
 import { Clipboard } from './clipboard';
 
 const meta: Meta<typeof Clipboard> = {
@@ -18,12 +19,38 @@ const meta: Meta<typeof Clipboard> = {
 type Story = StoryObj<typeof Clipboard>;
 
 /**
- * Default usage example of the Button component.
+ * Default usage example of the Clipboard component.
  */
 export const Default: Story = {
     args: {
         text: 'Sample text to copy...',
     },
+};
+
+export const WithLink: Story = {
+    args: {
+        text: 'http://example.com',
+        variant: 'avatar-white-bg',
+    },
+    render: (args) => (
+        <span className="flex items-center gap-3">
+            <Link href={args.text}>Link label</Link>
+            <Clipboard text={args.text} variant={args.variant} />
+        </span>
+    ),
+};
+
+export const WithText: Story = {
+    args: {
+        text: '0x123456789',
+        variant: 'avatar-white-bg',
+    },
+    render: (args) => (
+        <span className="flex items-center gap-3">
+            <p>{`${args.text.slice(0, 5)}...`}</p>
+            <Clipboard text={args.text} variant={args.variant} />
+        </span>
+    ),
 };
 
 export default meta;
