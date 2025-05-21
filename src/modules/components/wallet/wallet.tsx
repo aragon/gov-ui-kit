7e44c91562fb11c255881ab812600743890bcd69
Kevin Davis <whomst@kevind.xyz>
2025-05-21 10:02:44 +0200

    chore: fix skeleton padding

--- src/modules/components/wallet/wallet.tsx
+++ src/modules/components/wallet/wallet.tsx
@@ -38,17 +38,17 @@ export const Wallet: React.FC<IWalletProps> = (props) => {
         'flex max-w-44 items-center gap-3 rounded-full border border-neutral-100 bg-neutral-0 text-neutral-500 transition-all',
         'hover:border-neutral-200 active:bg-neutral-50 active:text-neutral-800',
         'focus:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-offset',
-        user == null ? 'px-4 py-2.5' : 'p-1',
-        isEnsLoading && 'pl-4',
+        { 'px-4 py-2.5': user == null },
+        { 'p-1 md:pl-4': user != null },
         className,
     );
 
-    const textBlockClass = classNames('truncate hidden pl-4', textClassName);
+    const textBlockClass = classNames('truncate hidden', textClassName);
 
     return (
         <button className={buttonClassName} {...otherProps}>
             {!user && copy.wallet.connect}
-            {user && isEnsLoading && <StateSkeletonBar size="lg" width={56} />}
+            {user && isEnsLoading && <StateSkeletonBar size="lg" width={56} className={textBlockClass} />}
             {user && !isEnsLoading && (
                 <span title={resolvedUserTitle} className={textBlockClass}>
                     {resolvedUserHandle}
