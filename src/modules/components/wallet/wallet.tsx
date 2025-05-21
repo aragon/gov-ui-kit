e5458fdde2f0af2522c8046861c71e1a44c916a9
Kevin Davis <whomst@kevind.xyz>
2025-05-21 14:06:14 +0200

    chore: resolve PR convos

--- src/modules/components/wallet/wallet.tsx
+++ src/modules/components/wallet/wallet.tsx
@@ -13,8 +13,8 @@ export interface IWalletProps extends React.ButtonHTMLAttributes<HTMLButtonEleme
      */
     user?: ICompositeAddress;
     /**
-     * Custom class name to control the text styles.
-     * This is useful for controlling visibility of the user handle with breakpoints. @default 'md:block'
+     * Custom class name to control the text styles, useful for controlling visibility of the user handle.
+     * @default 'md:block'
      */
     textClassName?: string;
 }
@@ -35,22 +35,21 @@ export const Wallet: React.FC<IWalletProps> = (props) => {
     const resolvedUserTitle = user?.name ?? ensName ?? user?.address;
 
     const buttonClassName = classNames(
-        'flex max-w-44 items-center gap-3 rounded-full border border-neutral-100 bg-neutral-0 text-neutral-500 transition-all',
-        'hover:border-neutral-200 active:bg-neutral-50 active:text-neutral-800',
-        'focus:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-offset',
+        'flex max-w-44 items-center gap-3 rounded-full border border-neutral-100 bg-neutral-0 text-neutral-500 transition-all cursor-pointer',
+        'hover:border-neutral-200 active:bg-neutral-50 active:text-neutral-800 disabled:cursor-default focus-ring-primary',
         { 'px-4 py-2.5': user == null },
         { 'p-1 md:pl-4': user != null },
         className,
     );
 
-    const textBlockClass = classNames('truncate hidden', textClassName);
+    const contentClassName = classNames('truncate hidden', textClassName);
 
     return (
         <button className={buttonClassName} {...otherProps}>
             {!user && copy.wallet.connect}
-            {user && isEnsLoading && <StateSkeletonBar size="lg" width={56} className={textBlockClass} />}
+            {user && isEnsLoading && <StateSkeletonBar size="lg" width={56} className={contentClassName} />}
             {user && !isEnsLoading && (
-                <span title={resolvedUserTitle} className={textBlockClass}>
+                <span title={resolvedUserTitle} className={contentClassName}>
                     {resolvedUserHandle}
                 </span>
             )}
