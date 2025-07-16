098517edfcee33318d6c8589edc90a65238dbf3b
shan8851 <asamshan456@gmail.com>
2025-07-16 12:20:16 +0100

    chore: update skeleton

--- src/modules/components/smartContract/smartContractFunctionDataListItem/smartContractFunctionDataListItemSkeleton/smartContractFunctionDataListItemSkeleton.tsx
+++ src/modules/components/smartContract/smartContractFunctionDataListItem/smartContractFunctionDataListItemSkeleton/smartContractFunctionDataListItemSkeleton.tsx
@@ -19,8 +19,13 @@ export const SmartContractFunctionDataListItemSkeleton: React.FC<ISmartContractF
             )}
             {...otherProps}
         >
-            <StateSkeletonBar width="50%" responsiveSize={{ md: 'lg' }} />
-            <StateSkeletonBar width="75%" responsiveSize={{ md: 'lg' }} />
+            <div className="flex w-1/2 md:w-1/3">
+                <StateSkeletonBar responsiveSize={{ md: 'lg' }} width="100%" />
+            </div>
+            <div className="flex w-3/4 gap-x-2 md:w-1/2">
+                <StateSkeletonBar responsiveSize={{ md: 'lg' }} width="50%" />
+                <StateSkeletonBar responsiveSize={{ md: 'lg' }} width="50%" />
+            </div>
         </DataList.Item>
     );
 };
