54cc47d6afa8b234508462f57b89fa1cb70ec847
Milos Dzepina <milos@aragon.org>
2025-08-26 17:12:41 +0200

    Add changeset
    Signed-off-by: Milos Dzepina <milos@aragon.org>

--- src/modules/components/proposal/index.ts
+++ src/modules/components/proposal/index.ts
@@ -1,5 +1,5 @@
-export * from './proposalActionSimulationStructure';
 export * from './proposalActions';
+export * from './proposalActionSimulationStructure';
 export * from './proposalDataListItem';
 export * from './proposalUtils';
 export * from './proposalVoting';
